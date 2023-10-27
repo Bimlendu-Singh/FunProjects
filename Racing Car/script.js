ed0583a84fcea410c2505f606ad776e45d2e502b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-10-27 14:56:26 +0530

    updated script and transformed it completely as per my design

--- Racing Car/script.js
+++ Racing Car/script.js
@@ -1,6 +1,13 @@
-var audio = document.createElement("audio");
-audio.setAttribute("src", "sound.mp3");
-audio.loop = true;
-audio.play();
+// var audio = document.createElement("audio");
+// audio.setAttribute("src", "sound.mp3");
+// audio.loop = true;
+// audio.play();
 
 // "D:\Education\Web Dev revisit\Fun Project\Racing Car\sound.mp3"
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var audio = document.createElement("audio");
+//   audio.setAttribute("src", "../sound.mp3");
+//   audio.loop = true;
+//   audio.play();
+// });
